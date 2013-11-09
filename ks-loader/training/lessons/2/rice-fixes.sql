bfb07e808672ff834a0e4ce530f633863fbddefc
cmann50 <cmann50@me.com>
2013-11-09 13:51:28 -0500

    SQL fixes

--- ks-loader/training/lessons/2/rice-fixes.sql
+++ ks-loader/training/lessons/2/rice-fixes.sql
@@ -16,16 +16,13 @@ SELECT KREW_RTE_NODE_S.NEXTVAL FROM dual;
 /
 ALTER SEQUENCE KREW_RTE_NODE_S INCREMENT BY 1;
 /
-
-
-
-- 
+ 
 -- Delete duplicate KIM types in rice data.  These are breaking the roles
 -- Screen since there are two KIM 'College Type' records:  101, 10008
 -- and two 'Department Type' records: 102, 10009
-delete from KRIM_TYP_T where KIM_TYP_ID='10008'
+delete from KRIM_TYP_T where KIM_TYP_ID='10008';
 /
-delete from KRIM_TYP_T where KIM_TYP_ID='10009'
+delete from KRIM_TYP_T where KIM_TYP_ID='10009';
 /
 
  
